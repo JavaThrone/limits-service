e26384ac6e1228aae9e35b2ac4983cdf051cf9d6
ApollonSkill <invaderzims@ukr.net>
2020-02-29 19:39:28 +0200

    Initial commit

---
+++ src/main/java/com/microservices/limitsservice/controller/LimitsConfigurationController.java
@@ -0,0 +1,21 @@
+package com.microservices.limitsservice.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.microservices.limitsservice.config.Configuration;
+import com.microservices.limitsservice.dto.LimitConfiguration;
+
+@RestController
+public class LimitsConfigurationController {
+
+    @Autowired
+    private Configuration configuration;
+
+    @GetMapping("/limits")
+    public LimitConfiguration retrieveLimitsFromConfigurations() {
+        return new LimitConfiguration(configuration.getMax(), configuration.getMin() + 6);
+    }
+
+}
